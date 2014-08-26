f66d4b9e15245b5b244dc514d4c5ff71db25219f
Joey Zhang <joeyzhang@163.com>
2014-08-26 21:25:07 +0800

    commit upload photo3

---
+++ WebContent/edit_photo3.jsp
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<%@page import="com.drift.servlet.MyServletUtil"%>
+<%@ page contentType="text/html; charset=utf8" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>修改头像-使用base64上传方式</title>
+<script src="http://open.web.meitu.com/sources/xiuxiu.js" type="text/javascript"></script>
+<script type="text/javascript">
+window.onload=function(){
+	xiuxiu.setLaunchVars("quality", 100);
+    xiuxiu.embedSWF("altContent",5,"100%","100%");
+       /*第1个参数是加载编辑器div容器，第2个参数是编辑器类型，第3个参数是div容器宽，第4个参数是div容器高*/
+    xiuxiu.setUploadType(3);
+    xiuxiu.setUploadURL("<%=MyServletUtil.entryURL%>" + "/upload3");
+    xiuxiu.onBeforeUpload = function(data, id){
+    	var size = data.size;
+    	if(size > 512 * 1024) {
+    		alert("图片不能超过512K");
+    		return false;
+    	}
+        xiuxiu.setUploadArgs({filetype: data.type, type: "image", filename: data.name });
+        return true;
+    }
+
+    xiuxiu.onSaveBase64Image = function (data, fileName, fileType, id) {
+    	alert("保存为base64图片，大小：" + data.length + "，文件名：" + fileName + "，类型：" + fileType);
+    }
+    
+    xiuxiu.onInit = function ()
+    {
+        xiuxiu.loadPhoto("");
+    }
+    xiuxiu.onUploadResponse = function(data)
+    {
+        if(data){
+            alert("头像设置成功");       
+            window.close();
+            //location.href="edit_profile"; 
+            window.opener.location.href="edit_profile";
+        }else{
+            alert("头像设置失败");    
+        }
+    }
+}
+</script>
+<style type="text/css">
+    html, body { height:100%; overflow:hidden; }
+    body { margin:0; }
+</style>
+</head>
+<body>
+<div id="altContent">
+    <h1>美图秀秀</h1>
+</div>
+</body>
+</html>
---
+++ src/com/drift/api/ApiUploadPhoto2.java
@@ -0,0 +1,77 @@
+package com.drift.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.drift.core.DBConnector;
+import com.drift.util.JSONUtil;
+import com.drift.util.PhotoUtil;
+
+/**
+ * Servlet implementation class ApiUploadPhoto2
+ */
+@WebServlet(ApiController.API_ROOT + "/upload_photo2")
+public class ApiUploadPhoto2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiUploadPhoto2() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ApiController.doCommonTasks(request, response);
+		int status = ApiController.API_ERR_OTHER;
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		String uidStr = request.getParameter("uid");
+		if(uidStr == null) {
+			status = ApiController.API_ERR_BAD_ARGS;
+		} else {
+			int uid = 0;
+			try {
+				uid = Integer.parseInt(uidStr);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			if(DBConnector.checkUser(uid) == false) {
+				status = ApiController.API_ERR_BAD_USER_ID;
+			} else {
+				String path = getServletContext().getRealPath("/photo"); //上传文件目录
+				//System.out.println(path);
+				int dbStatus = PhotoUtil.uploadPhoto2(request, path, uid);
+				status = ApiController.mapDBCode(dbStatus);
+			}
+		}
+		String msg = ApiController.API_CODE_STRINGS.get(status);
+		map.put("code", status);
+		map.put("result", msg);
+		//System.out.println(status);
+
+		PrintWriter out = response.getWriter();
+		out.print(JSONUtil.toJSONString(map));
+		out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
